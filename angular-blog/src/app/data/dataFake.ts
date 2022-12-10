c4026c7abc88fc00d3b52c7440fca348c93138a1
uzielmenezes <uzielcivil@gmail.coom>
2022-12-10 10:55:17 -0300

    fake data

---
+++ angular-blog/src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    id: 1,
+    image: 'aaa',
+    title: 'bla bla bla',
+    description: 'bla bla bla bla bla bla',
+  },
+  {
+    id: 2,
+    image: 'aaa',
+    title: 'bla bla bla',
+    description: 'bla bla bla bla bla bla',
+  },
+  {
+    id: 3,
+    image: 'aaa',
+    title: 'bla bla bla',
+    description: 'bla bla bla bla bla bla',
+  },
+  {
+    id: 4,
+    image: 'aaa',
+    title: 'bla bla bla',
+    description: 'bla bla bla bla bla bla',
+  },
+];
