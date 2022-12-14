fe3b49a4a151af7e934457e827acc95ff4729595
uzielmenezes <uzielcivil@gmail.coom>
2022-12-14 07:54:52 -0300

    component properties

--- projeto-buzzfeed/src/app/componwnts/quiz/quiz.component.ts
+++ projeto-buzzfeed/src/app/componwnts/quiz/quiz.component.ts
@@ -3,13 +3,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.component.html',
-  styleUrls: ['./quiz.component.css']
+  styleUrls: ['./quiz.component.css'],
 })
 export class QuizComponent implements OnInit {
+  title: string = 'QUIZ';
 
-  constructor() { }
+  questions: any;
+  questionSelected: any;
 
-  ngOnInit(): void {
-  }
+  answers: string[] = [];
+  answerSelected: string = '';
 
+  questionIndex: number = 0;
+  questionMaxIndex: number = 0;
+
+  finished: boolean = false;
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
