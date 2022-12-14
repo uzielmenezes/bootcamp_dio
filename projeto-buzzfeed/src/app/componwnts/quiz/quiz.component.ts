a44b6569b7c565f9f43713b8c8d53b1d513bd915
uzielmenezes <uzielcivil@gmail.coom>
2022-12-14 12:48:55 -0300

    methods to store answers and give the final result

--- projeto-buzzfeed/src/app/componwnts/quiz/quiz.component.ts
+++ projeto-buzzfeed/src/app/componwnts/quiz/quiz.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import quiz_questions from './../../../assets/data/quiz_questions.json';
+
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.component.html',
@@ -19,7 +21,46 @@ export class QuizComponent implements OnInit {
 
   finished: boolean = false;
 
+  response: string = '';
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (quiz_questions) {
+      this.title = quiz_questions.title;
+
+      this.questions = quiz_questions.questions;
+      this.questionSelected = this.questions[this.questionIndex];
+
+      this.questionMaxIndex = this.questions.length;
+    }
+  }
+
+  playerChoice(value: string) {
+    this.answers.push(value);
+    console.log(this.answers);
+    this.nextStep();
+  }
+
+  nextStep() {
+    this.questionIndex += 1;
+    if (this.questionMaxIndex > this.questionIndex) {
+      this.questionSelected = this.questions[this.questionIndex];
+    } else {
+      this.finished = true;
+    }
+    this.result();
+  }
+
+  result() {
+    if (this.finished) {
+      let villain = this.answers.filter((x) => x === 'A').length;
+      let hero = this.answers.filter((x) => x === 'B').length;
+      if (villain > hero) {
+        this.response = quiz_questions.results.A;
+      } else {
+        this.response = quiz_questions.results.B;
+      }
+    }
+  }
 }
