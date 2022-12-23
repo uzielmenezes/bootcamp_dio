dff7c907d0251c6bfc172dc76ba2afbc6a1c139b
uzielmenezes <uzielcivil@gmail.coom>
2022-12-23 07:55:07 -0300

    Dev class methods developed

--- projetos-java/desafio-dio-POO/src/br/com/dio/desafio/dominio/Dev.java
+++ projetos-java/desafio-dio-POO/src/br/com/dio/desafio/dominio/Dev.java
@@ -2,6 +2,7 @@ package br.com.dio.desafio.dominio;
 
 import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
@@ -11,12 +12,22 @@ public class Dev {
     private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
 
     public void inscreverBootcamp(BootCamp bootcamp) {
+        bootcamp.getDevsInscritos().add(this);
+        this.conteudosInscritos.addAll(bootcamp.getConteudos());
     }
 
     public void progredir() {
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+        if (conteudo.isPresent()) {
+            this.conteudosConcluidos.add(conteudo.get());
+            this.conteudosInscritos.remove(conteudo.get());
+        } else {
+            System.err.println("Você não está matriculado em nenhum conteúdo!");
+        }
     }
 
-    public void calcularTotalXp() {
+    public double calcularTotalXp() {
+        return this.conteudosConcluidos.stream().mapToDouble(Conteudo::calcularXp).sum();
     }
 
     public String getNome() {
