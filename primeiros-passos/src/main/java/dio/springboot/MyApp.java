bb00a24e37b4fd4adaeebc1cef6819828f85053f
uzielmenezes <uzielcivil@gmail.coom>
2023-01-09 12:28:36 -0300

    new springboot project

---
+++ primeiros-passos/src/main/java/dio/springboot/MyApp.java
@@ -0,0 +1,15 @@
+package dio.springboot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyApp implements CommandLineRunner {
+    @Autowired
+    private Calculator calculator;
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("O resultado é: " + calculator.sum(7,8));
+    }
+}
